7eaf5173b855758f04e0ee0fe896e34586b55126
krychuu01 <72739801+krychuu01@users.noreply.github.com>
2021-10-06 18:43:25 +0200

    Pierwszy lab

---
+++ src/pl/kropiak/po/lab00/zad6.java
@@ -0,0 +1,14 @@
+package pl.kropiak.po.lab00;
+
+public class zad6 {
+    public static void main(String[] args){
+        System.out.println("  ^^/////^^");
+        System.out.println("( --- --- )");
+        System.out.println("( ### @@@ )");
+        System.out.println("(    /    )");
+        System.out.println("(    |    )");
+        System.out.println(" \\   o   /");
+        System.out.println("  ------");
+
+    }
+}
